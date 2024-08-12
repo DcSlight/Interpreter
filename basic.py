defde492ba8e12ce72af5f0e55a88cb9392ca09e
Idan Noyshul <99333543+DcSlight@users.noreply.github.com>
2024-08-12 23:05:16 +0300

    change to class

---
+++ basic.py
@@ -0,0 +1,28 @@
+from Lexer import  Lexer
+from Parser import Parser
+from interpreter import Interpreter
+from interpreter import Context
+
+
+
+#######################################
+# RUN
+#######################################
+
+def run(fn, text):
+    # Generate tokens
+    lexer = Lexer(fn, text)
+    tokens, error = lexer.make_tokens()
+    if error: return None, error #TODO: change
+
+    # Generate AST
+    parser = Parser(tokens)
+    ast = parser.parse()
+    if ast.error: return None, ast.error
+
+    # Run program
+    interpreter = Interpreter()
+    context = Context('<program>')
+    result = interpreter.visit(ast.node, context)
+
+    return result.value, result.error
